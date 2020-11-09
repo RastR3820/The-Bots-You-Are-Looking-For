80ee2669750359fd44bad95cc09784bfcf319e6f
teresathyle <45610502+teresathyle@users.noreply.github.com>
2020-11-09 16:49:45 -0600

    Update home.py

--- home.py
+++ home.py
@@ -2,7 +2,7 @@ from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
-@app.route("/<name>")
+@app.route("/>")
 def home(name):
 	return render_template("index.html")
 
