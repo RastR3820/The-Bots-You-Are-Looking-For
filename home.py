346362da5d1bb10f1b72eb3334cc86116c757b02
teresathyle <45610502+teresathyle@users.noreply.github.com>
2020-11-10 01:39:17 -0600

    Update home.py

--- home.py
+++ home.py
@@ -1,11 +1,20 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, request, url_for, render_template
 
 app = Flask(__name__)
 
-@app.route("/>")
-def home(name):
-	return render_template("index.html")
+@app.route("/", methods=['POST','GET'])
+def home():
+    if request.method == 'POST':
+        credentials = request.form
+        username = credentials["username"]
+        password = credentials["password"]
+        return username
+    
+    return render_template("index.html", content="Testing")
+
+@app.route("/advising-chatbot")
+def chatbot():
+    return render_template("chatbot.html")
 
 if __name__ == "__main__":
-	app.run()
-
+    app.run()
